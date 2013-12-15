4761d46dde98731a3e2c41ee9a23d6eb790bc9df
lvjian700 <lvjian700@gmail.com>
2013-12-16 06:41:40 +0800

    1.11 ~ 1.15

--- chapter-1/1.11.lisp
+++ chapter-1/1.11.lisp
@@ -1,1 +1,27 @@
-(define fn-recursion 
+(define (fn-recursion n)
+  (cond ((< n 3) n)
+		(else (+ (* 1 (fn-recursion (- n 1)))
+				 (* 2 (fn-recursion (- n 2)))
+				 (* 3 (fn-recursion (- n 3))))
+			  )))
+
+(fn-recursion 2)
+(fn-recursion 20)
+
+(define (fn n)
+  (iter 2 1 0 0 n))
+
+(define (iter a b c i n) 
+  (if (= i n)
+	c
+	(iter ((+ a (* 2 b) (* 3 c))
+		   a
+		   b
+		   (+ i 1)
+		   n))
+	)
+)
+
+
+(fn 2)
+(fn 20)
